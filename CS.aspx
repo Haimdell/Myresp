6aae3c8f547251a2acf668e2f3ece6d9c57c247e
Haimdell <last1111111@mail.ru>
2016-04-08 10:45:31 +0300

    Added files via upload

---
+++ CS.aspx
@@ -0,0 +1,22 @@
+﻿
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CS.aspx.cs" Inherits="CS" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<asp:textbox id="text1" Text="Имя сервера" runat="server"></asp:textbox>
+<asp:textbox id="text2"  Text="Имя базы данных" runat="server"></asp:textbox>
+        <br><br>
+<asp:textbox id="text3"  Text="Логин" runat="server"></asp:textbox>
+<asp:textbox id="text4"  Text="Пароль" runat="server"></asp:textbox>
+         <br><br>
+        <asp:textbox id="text5"  Text="Имя таблицы" runat="server"></asp:textbox>
+    <asp:Button Text="Export" OnClick="ExportCSV" runat="server" />
+    </form>
+</body>
+</html>
---
+++ CS.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+//Add MySql Library
+using MySql.Data.MySqlClient;
+
+
+public partial class CS : System.Web.UI.Page
+{
+    public static int val = 0;
+    private MySqlConnection connection;
+    private string server;
+    private string database;
+    private string uid;
+    private string password;
+
+    //Constructor
+    /*public CS()
+    {
+        Initialize();
+    }*/
+
+    //Initialize values
+    private void Initialize()
+    {
+       
+        server = text1.Text;
+      
+        database = text2.Text;
+       
+        uid = text3.Text;
+     
+        password = text4.Text;
+        string connectionString;
+        connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        connection = new MySqlConnection(connectionString);
+    }
+
+
+    //open connection to database
+    private bool OpenConnection()
+    {
+        try
+        {
+            connection.Open();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+            //When handling errors, you can your application's response based on the error number.
+            //The two most common error numbers when connecting are as follows:
+            //0: Cannot connect to server.
+            //1045: Invalid user name and/or password.
+            switch (ex.Number)
+            {
+                case 0:
+                   break;
+                case 1045:
+                    break;
+            }
+            return false;
+        }
+    }
+
+    //Close connection
+    private bool CloseConnection()
+    {
+        try
+        {
+            connection.Close();
+            return true;
+        }
+        catch (MySqlException ex)
+        {
+           
+            return false;
+        }
+    }
+
+    protected void ExportCSV(object sender, EventArgs e)
+    {
+        Initialize();
+
+        List<string>[] list;
+        list = Select();
+
+        string path = @"D:\MyTest.csv";
+       
+        for (int k = 0; k < list[0].Count; k++)
+        {
+            if (k == k)
+            { File.AppendAllText(path, "\n", Encoding.UTF8); }
+
+            for (int i2 = 0; i2 < val; i2++)
+            {
+
+                File.AppendAllText(path, list[i2][k] + ";", Encoding.UTF8);
+            }
+        }
+    }
+        
+        public List<string>[] Select()
+    {
+        string query = "SELECT * FROM " + "`" + text5.Text+ "`";
+
+        //Create a list to store the result
+        List<string>[] list = new List<string>[8];
+        for (int i = 0; i < 8; i++)
+        {
+            list[i] = new List<string>();
+        }
+
+        string path2 = @"D:\MyTest.csv";
+       
+        //Open connection
+        if (this.OpenConnection() == true)
+        {
+            //Create Command
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            
+            //Create a data reader and Execute the command
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            
+
+            //Read the data and store them in the list
+            while (dataReader.Read())
+            {
+
+                for (int b = 0; b < dataReader.FieldCount; b++)
+                {
+                    ;
+                    list[b].Add(dataReader[b] + "");
+                    val = dataReader.FieldCount;
+                }
+            }
+
+            //close Data Reader
+            dataReader.Close();
+
+            //close Connection
+            this.CloseConnection();
+
+            //return list to be displayed
+            return list;
+        }
+        else
+        {
+            return list;
+        }
+    }
+}
+
+
